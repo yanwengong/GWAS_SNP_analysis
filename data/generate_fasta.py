6cc4b00892efaaaa22e1d2fa5636b8f52a4728fe
Yanwen Gong <yanwengong7@gmail.com>
2021-07-17 22:55:29 -0700

    update to take combined peak and snp as input, allow analsis for all SNPs

--- data/generate_fasta.py
+++ data/generate_fasta.py
@@ -6,19 +6,19 @@ import numpy as np
 
 
 class GetFasta():
-    def __init__(self, peak_bed, snp_bed, ref_fasta):
-        self.peak_bed = peak_bed
-        self.snp_bed = snp_bed
+    def __init__(self, peak_snp_bed, ref_fasta):
+        self.peak_snp_bed = peak_snp_bed
         self.ref_fasta = ref_fasta
 
     # read in the bed
     def read_bed(self):
-        peak_bed_df = pd.read_csv(self.peak_bed, sep="\t", header=None)
-        snp_bed_df = pd.read_csv(self.snp_bed, sep="\t", header=None)
-        combined = pd.concat([peak_bed_df, snp_bed_df], axis=1)
+        combined = pd.read_csv(self.peak_snp_bed, sep="\t", header=None)
+        # snp_bed_df = pd.read_csv(self.snp_bed, sep="\t", header=None)
+        # combined = pd.concat([peak_bed_df, snp_bed_df], axis=1)
+
         combined.columns = ['peak_chr', 'peak_start', 'peak_end',
                             'snp_chr', 'snp_start', 'snp_end',
-                            'snp_id', 'wt', 'mutant']
+                            'snp_id', 'Effect_Allele', 'Allele']
 
         # check if the the snp is within peak
         if combined['peak_chr'].equals(combined['snp_chr']) and \
@@ -26,9 +26,9 @@ class GetFasta():
                 all(combined['peak_end'] >= combined['snp_end']):
             pass
         else:
-            raise ValueError('Given WT nucleic acid does not match with the one retrieve in fasta ')
+            raise ValueError('SNP is not within peak')
 
-        return peak_bed_df, snp_bed_df, combined
+        return combined
 
     # compute the location index of snp within the peak
     def compute_pos_index(self, combined):
@@ -37,7 +37,7 @@ class GetFasta():
     # generate wt and mutant fasta
     # TODO check why the snp info and the returned nucliec does not match - returned one pos further
     def get_fasta(self, snp_pos, combined):
-        peak_bed_bt = BedTool(self.peak_bed)
+        peak_bed_bt = BedTool(self.peak_snp_bed)
         ref_bt = BedTool(self.ref_fasta)
         peak_fa = peak_bed_bt.sequence(fi=ref_bt)
         peak_fa = open(peak_fa.seqfn).read()
@@ -51,7 +51,7 @@ class GetFasta():
         index_list = [i*2+1 for i in range(n_peak)]
         peak = peak_fa.split("\n")
         peak_list = [peak[i].upper() for i in index_list] # TODO: change to upper
-        print(peak_list)
+        #print(peak_list)
         return peak_list
 
     def _generate_mutant_fa(self, combined, peak_fa_list):
@@ -66,12 +66,24 @@ class GetFasta():
             fa = peak_fa_list[i]
             snp_index = snp_index_list[i]
 
-            if fa[snp_index] != combined.iloc[i, 7]:
-                raise ValueError('Given WT nucleic acid does not match with the one retrieve in fasta')
-            else:
+            ## WT is Effect_Allele
+            if fa[snp_index] == combined.iloc[i, 7]:
                 fa = list(fa)
                 fa[snp_index] = combined.iloc[i, 8].upper()
                 mutant_fa = ''.join(fa)
                 mutant_peak_fa_list.append(mutant_fa)
+            ## WT is Allele
+            elif fa[snp_index] == combined.iloc[i, 8]:
+                fa = list(fa)
+                fa[snp_index] = combined.iloc[i, 7].upper()
+                mutant_fa = ''.join(fa)
+                mutant_peak_fa_list.append(mutant_fa)
+
+            else:
+                print(fa)
+                print(fa[snp_index])
+                print(combined.iloc[i, :])
+
+                raise ValueError('Given WT nucleic acid does not match with the one retrieve in fasta')
 
         return mutant_peak_fa_list
